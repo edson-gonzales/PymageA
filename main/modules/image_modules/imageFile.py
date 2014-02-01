3c2d9f923768dfb93de5f479f7b50b9b4f28dc07
Veronica Prado <veronica.prado@jalasoft.com>
2014-01-31 20:15:27 -0400

    update main/modules/image_modules/imageFile.py

--- main/modules/image_modules/imageFile.py
+++ main/modules/image_modules/imageFile.py
@@ -1,3 +1,5 @@
+import imghdr
+import os
 class image_file:
 	file_name=''
 	file_size=''
@@ -10,6 +12,19 @@ class image_file:
 
 	def __init__(self):
 		self.file_path="" #root path by default-windows
+
+	def set_image_values(self,full_path_file_image,full_name_file_image): 
+		self.set_path(os.path.abspath(full_path_file_image))
+		split_name_image=os.path.splitext(full_name_file_image)
+		self.set_name(split_name_image[0])
+		self.set_type(split_name_image[1].translate(None,'.'))#remove . from extension file
+
+
+	def get_complete_image_with_type(self):
+		return (self.get_name()+"."+self.get_type())
+
+	def get_full_path_with_name_image_type(self):
+		return (os.path.join(self.get_path(),self.get_complete_image_with_type()))
 
 	def get_name(self):
 		return self.file_name
@@ -45,6 +60,15 @@ class image_file:
 
 	def get_path(self):
 		return self.file_path
+
+	def get_full_file_name(self):
+		return self.file_path+"\\"+self.file_name+"."+self.file_type
+
+	def is_image_PNG(self):
+		if((imghdr.what(self.get_full_file_name()))).upper()=="PNG":
+			return True
+		else:
+			return False
 
 	
 
--- tests/test_modules/test_image/image_file_test.py
+++ tests/test_modules/test_image/image_file_test.py
@@ -1,12 +1,12 @@
 import unittest
 import sys
 import os.path
-from main.modules.Image_modules.Image_modules import ImageFile
+from main.modules.image_modules.imageFile import image_file
 
 class ManageImage(unittest.TestCase):
 	def test_is_validate_name(self):
 		pass
-		# self.assertEquals("", get_name(set_name( )))
+		#self.assertEquals("", get_name(set_name( )))
 		
 
 		
