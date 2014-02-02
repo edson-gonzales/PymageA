35a7608b960c6a368e450d5a4a329cb8eda5daba
Yasmina Terrazas <yasmina.terrazas@jalasoft.com>
2014-02-01 22:47:05 -0400

    Add more unit tests and functions for list images

--- main/modules/list_images_module/list_images.py
+++ main/modules/list_images_module/list_images.py
@@ -1,11 +1,46 @@
 import os
 
 class ListImages():
-	images = [] 
+	"""Class ListImages handles the files retrieved from a given path and add them to an array"""
 	
+	images = [] 
+	image_file_types = [] # The supported file types 
+	
+	def __init__(self):
+		self.image_file_types = ['.jpg', '.png', '.bmp']
+	
+	def is_file_in_image_scope(self, extension):
+		"""Verify extension is contained in files supported and return a True/False """
+		
+		it_is = False
+		if extension in self.image_file_types:
+			it_is = True
+		return it_is
+			
+		
 	def get_all_images_from_path(self, given_path):
-		list_of_files = os.listdir(given_path)
-		return list_of_files
-
-
+		"""Add all images contained in directories and subdirectories to a list and returns the list"""
+		
+		list_of_directories = self.get_all_nested_directories(given_path) 
+		list_of_images = []
+		for directory in list_of_directories:
+			list_of_files = os.listdir(directory)
+			for file_name in list_of_files:
+				file_base_name, file_extension = os.path.splitext(file_name)
+				if self.is_file_in_image_scope(file_extension):
+					list_of_images.append(file_name)
+		
+		return list_of_images
+	
+	def get_all_nested_directories(self, given_path):
+		""" Retrieves all nested directories from a path and return them in a list"""
+		
+		list_of_directories_full_path = []
+		for root, dirs, files in os.walk(given_path):
+			list_of_directories_full_path.append(root)
+		return list_of_directories_full_path
+	
+	
+			
+				
 		
--- tests/test_modules/test_list_images/test_list_images.py
+++ tests/test_modules/test_list_images/test_list_images.py
@@ -2,24 +2,46 @@ import unittest
 import sys
 import os.path
 
-main_path = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir, os.path.pardir, os.path.pardir))
-path_to_add = main_path + "\\main\\modules\\list_images_module" 
-sys.path.append(path_to_add)
-
-
-from list_images import ListImages
+sys.path.append("../../../")
+from main.modules.list_images_module.list_images import ListImages
 
 class TestListImages(unittest.TestCase):
 	
+	list_images = ''
+	test_path = ''
+	
+	def setUp(self):
+		self.list_images = ListImages()
+		self.test_path, not_needed_current_dir = os.path.split(os.getcwd())
 			
-	def test_list_images_of_specified_directory(self):
-		pass
-		list_images = ListImages()
-		test_path, not_neede_current_dir = os.path.split(os.getcwd())
-		test_path = test_path + "\\images_for_unittest"
+	def test_list_images_in_specified_scope_of_specified_directory(self):
+		self.test_path = self.test_path + "/images_for_unittest"
 		list_of_current_images_on_directory = ['Image1.jpg','Image2.png','Image3.bmp']
-		list_of_images_from_get_all_images_from_home = list_images.get_all_images_from_path(test_path)
-		self.assertEquals(list_of_current_images_on_directory,list_of_images_from_get_all_images_from_home )
+		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_path(self.test_path)
+		self.assertEquals(list_of_current_images_on_directory,list_of_images_from_get_all_images_from_path )
+		
+	def test_list_only_images_in_specified_scope_of_specified_directory(self):
+		self.test_path = self.test_path + "/images2_for_unittest"
+		list_of_current_images_on_directory = ['Image1.jpg','Image2.png','Image3.bmp']
+		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_path(self.test_path)
+		self.assertEquals(list_of_current_images_on_directory,list_of_images_from_get_all_images_from_path )	
+	
+	def test_is_file_in_image_scope_return_true_for_jpg(self):
+		return self.assertTrue(self.list_images.is_file_in_image_scope('.jpg'))
+		
+	def test_list_images_in_specified_scope_of_specified_directory_and_nested_directories(self):
+		self.test_path = self.test_path + "/images3_for_unittest"
+		list_of_current_images_on_directory = ['Image4.jpg','Image1.jpg','Image2.png','Image3.bmp','Image1.jpg','Image2.png','Image3.bmp','Image1.jpg','Image2.png','Image3.bmp']
+		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_path(self.test_path)
+		self.assertItemsEqual(list_of_current_images_on_directory,list_of_images_from_get_all_images_from_path )	
+				
+	def test_list_images_returns_images_and_not_directories(self):
+		self.test_path = self.test_path + "/images4_for_unit_test"
+		list_of_current_images_on_nested_directories = ['Image1.jpg','Image2.png','Image3.bmp']
+		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_path(self.test_path)
+		self.assertEquals(list_of_current_images_on_nested_directories,list_of_images_from_get_all_images_from_path )
+	
+	
 		
 if __name__ == '__main__':
 	unittest.main()
