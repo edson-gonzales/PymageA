4ca1dcc52e07fc1bb301941463d8f07bd6ffd63e
Yasmina Terrazas <yasmina.terrazas@jalasoft.com>
2014-02-04 17:07:04 -0400

    Add documentation to the code and apply coding standars

--- main/modules/list_images_module/list_images.py
+++ main/modules/list_images_module/list_images.py
@@ -6,24 +6,40 @@ class ListImages():
 	images = [] 
 	image_file_types = [] # The supported file types 
 	
+	
 	def __init__(self):
 		self.image_file_types = ['.jpg', '.png', '.bmp']
 	
 	def is_file_in_image_scope(self, extension):
-		"""Verify extension is contained in files supported and return a True/False """
+		"""Verify extension is contained in files supported and return a True/False 
+		
+		Keyword arguments:
+		extension -- Given extension of a file
+		
+		"""
 		
 		it_is = False
 		if extension in self.image_file_types:
 			it_is = True
+		
 		return it_is
-			
+		
 		
 	def get_all_images_from_directory(self, size, list_of_images, list_of_directories):
-		"""Add all images contained in directories and subdirectories to a list and returns the list"""
+		"""Add all images contained in the list of directories received to a list and and returns that list.
+			It receives a list of directories and in a recursive way list all images contained on those.
+			
+		Keyword arguments:
+		size -- The current size of the elements not visited yet. 
+				Initial value will be the same as the lenght of the list of directories received 
+		list_of_images -- The list of images collected from the directories. 
+							This will grow with each recursive call to the method
+		list_of_directories -- The list of all directories where it needs to look for. 
+								This list will be the same always. 
 		
-		#list_of_directories = self.get_all_nested_directories(given_path) 
-		#list_of_images = []
-		next_folder_in_array = len(list_of_directories) - size
+		"""
+		
+		next_folder_in_array = len(list_of_directories) - size # To step up on each position of the directory list 
 		if next_folder_in_array >= len(list_of_directories):
 			return list_of_images
 		else:
@@ -36,13 +52,18 @@ class ListImages():
 					
 			return (self.get_all_images_from_directory(size, list_of_images, list_of_directories))
 		
-	
 	def get_all_nested_directories(self, given_path):
-		""" Retrieves all nested directories from a path and return them in a list"""
+		""" Retrieves all nested directories from a path and return them in a list
+			
+		Keyword arguments:
+		given_path -- The top level folder we want to extract the list of folders.
+		
+		"""
 		
 		list_of_directories_full_path = []
 		for root, dirs, files in os.walk(given_path):
 			list_of_directories_full_path.append(root)
+		
 		return list_of_directories_full_path
 	
 	
--- tests/test_modules/test_list_images/test_list_images.py
+++ tests/test_modules/test_list_images/test_list_images.py
@@ -9,7 +9,6 @@ class TestListImages(unittest.TestCase):
 	
 	list_images = ''
 	test_path = ''
-	size_of_list_of_folders = ''
 	list_of_directories = []
 	
 	
@@ -21,21 +20,21 @@ class TestListImages(unittest.TestCase):
 		list_of_images = []
 		self.test_path = self.test_path + "/images_for_unittest"
 		self.list_of_directories = self.list_images.get_all_nested_directories(self.test_path)
-		self.size_of_list_of_folders = len(self.list_of_directories)
-		list_of_current_images_on_directory = ['Image1.jpg','Image2.png','Image3.bmp']
-		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_directory \
-														(self.size_of_list_of_folders, list_of_images, self.list_of_directories, )
-		self.assertEquals(list_of_current_images_on_directory,list_of_images_from_get_all_images_from_path )
+		size_of_list_of_folders = len(self.list_of_directories)
+		list_of_images_expected = ['Image1.jpg','Image2.png','Image3.bmp']
+		list_of_images_obtained = self.list_images.get_all_images_from_directory \
+								(size_of_list_of_folders, list_of_images, self.list_of_directories)
+		self.assertEquals(list_of_images_expected, list_of_images_obtained)
 		
 	def test_list_only_images_in_specified_scope_of_specified_directory(self):
 		list_of_images = []
 		self.test_path = self.test_path + "/images2_for_unittest"
 		self.list_of_directories = self.list_images.get_all_nested_directories(self.test_path)
-		self.size_of_list_of_folders = len(self.list_of_directories)
-		list_of_current_images_on_directory = ['Image1.jpg','Image2.png','Image3.bmp']
-		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_directory \
-														(self.size_of_list_of_folders, list_of_images, self.list_of_directories, )
-		self.assertItemsEqual(list_of_current_images_on_directory,list_of_images_from_get_all_images_from_path )	
+		size_of_list_of_folders = len(self.list_of_directories)
+		list_of_images_expected = ['Image1.jpg','Image2.png','Image3.bmp']
+		list_of_images_obtained = self.list_images.get_all_images_from_directory \
+								(size_of_list_of_folders, list_of_images, self.list_of_directories)
+		self.assertItemsEqual(list_of_images_expected, list_of_images_obtained)	
 	
 	def test_is_file_in_image_scope_return_true_for_jpg(self):
 		return self.assertTrue(self.list_images.is_file_in_image_scope('.jpg'))
@@ -44,23 +43,25 @@ class TestListImages(unittest.TestCase):
 		list_of_images = []
 		self.test_path = self.test_path + "/images3_for_unittest"
 		self.list_of_directories = self.list_images.get_all_nested_directories(self.test_path)
-		self.size_of_list_of_folders = len(self.list_of_directories)
-		list_of_current_images_on_directory = ['Image4.jpg','Image1.jpg','Image2.png',\
-								'Image3.bmp','Image1.jpg','Image2.png','Image3.bmp','Image1.jpg','Image2.png','Image3.bmp']
-		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_directory \
-														(self.size_of_list_of_folders, list_of_images, self.list_of_directories,)
-		self.assertItemsEqual(list_of_current_images_on_directory,list_of_images_from_get_all_images_from_path )	
-				
+		size_of_list_of_folders = len(self.list_of_directories)
+		list_of_images_expected = ['Image4.jpg','Image1.jpg','Image2.png',\
+								   'Image3.bmp','Image1.jpg','Image2.png',\
+								   'Image3.bmp','Image1.jpg','Image2.png',\
+								   'Image3.bmp'\
+									]
+		list_of_images_obtained = self.list_images.get_all_images_from_directory \
+								(size_of_list_of_folders, list_of_images, self.list_of_directories)
+		self.assertItemsEqual(list_of_images_expected, list_of_images_obtained)	
+	
 	def test_list_images_returns_images_and_not_directories(self):
 		list_of_images = []
 		self.test_path = self.test_path + "/images4_for_unit_test"
 		self.list_of_directories = self.list_images.get_all_nested_directories(self.test_path)
-		self.size_of_list_of_folders = len(self.list_of_directories)
-		list_of_current_images_on_nested_directories = ['Image1.jpg','Image2.png','Image3.bmp']
-		list_of_images_from_get_all_images_from_path = self.list_images.get_all_images_from_directory \
-														(self.size_of_list_of_folders, list_of_images, self.list_of_directories)
-		self.assertItemsEqual(list_of_current_images_on_nested_directories,list_of_images_from_get_all_images_from_path )
-	
+		size_of_list_of_folders = len(self.list_of_directories)
+		list_of_images_expected = ['Image1.jpg','Image2.png','Image3.bmp']
+		list_of_images_obtained = self.list_images.get_all_images_from_directory \
+								(size_of_list_of_folders, list_of_images, self.list_of_directories)
+		self.assertItemsEqual(list_of_images_expected, list_of_images_obtained)
 	
 		
 if __name__ == '__main__':
