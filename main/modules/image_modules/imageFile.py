e1fc982ae72dbf2e8eb8bc948804057af8bce9ab
Veronica Prado <veronica.prado@jalasoft.com>
2014-02-14 22:29:09 -0400

    updated corrections to imageFile

--- main/modules/image_modules/imageFile.py
+++ main/modules/image_modules/imageFile.py
@@ -53,37 +53,54 @@ class ImageFile():
 			info_file_image = os.stat(image_full_path_name)
 			self.size_KB_image = info_file_image.st_size #size of image file
 			self.set_size_pixels(new_image.size[1], new_image.size[0])
-			self.set_file_onwer(full_path_file_image, full_name_file_image)
-		
-			
+			self.set_file_onwer(full_path_file_image, full_name_file_image)			
 			return True
-		
 		return False
+
 	def set_file_onwer(self, full_path_file_image, full_name_file_image):
 		if (platform.system() == "Linux"):
-			image_full_path_name = os.path.abspath(full_path_file_image + full_name_file_image)
-			info_file_image = os.stat(image_full_path_name)
-			uid = info_file_image.st_uid
-			self.file_owner = pwd.getpwuid(uid).pw_name
+			self.set_file_owner_in_platform_linux(full_path_file_image, full_name_file_image)
 		if platform.system() == 'Windows' :
-			if full_path_file_image.endswith('/') == True or full_path_file_image.endswith('\\') == True:
-				full_path_file_image = full_path_file_image[:-1]
-			full_path_file_image = os.path.abspath(full_path_file_image)
-			mypath = os.path.dirname(os.path.abspath(__file__))
-			new_name = os.path.dirname(os.path.abspath(__file__)) + '/delete' + full_name_file_image + '.txt'
-			new_name = os.path.abspath (new_name)
-			command_windows = 'dir /q ' + full_path_file_image + '\\' + full_name_file_image + '>' + new_name
-			info_file = os.system(command_windows)
-			try:
-				file_info = open(new_name)
-				lines = file_info.readlines()
-				info_file_owner = (lines[5].rstrip("\n")).split(' ')
-				self.file_owner = (info_file_owner[len(info_file_owner)-2]).split("\\")
-				file_info.close()
-				os.remove(new_name)
-			except IOError:
-				print("File cannot open" + new_name)
+			self.set_file_owner_in_platform_Windows(full_path_file_image, full_name_file_image)
 		
+
+	def set_file_owner_in_platform_linux(self, full_path_file_image, full_name_file_image):
+		""" Set owner to imageFile  when project is running in platform Linux
+
+		Keyword arguments:
+		full_path_file_image -- path were image is located
+		full_name_file_image -- file name image (with extension) 
+		"""
+		image_full_path_name = os.path.abspath(full_path_file_image + full_name_file_image)
+		info_file_image = os.stat(image_full_path_name)
+		uid = info_file_image.st_uid
+		self.file_owner = pwd.getpwuid(uid).pw_name
+
+	def set_file_owner_in_platform_Windows(self, full_path_file_image, full_name_file_image):
+		""" Set owner to imageFile  when project is running in platform Windows
+
+		Keyword arguments:
+		full_path_file_image -- path were image is located
+		full_name_file_image -- file name image (with extension) 
+
+		"""
+		if full_path_file_image.endswith('/') == True or full_path_file_image.endswith('\\') == True:
+				full_path_file_image = full_path_file_image[:-1]
+		full_path_file_image = os.path.abspath(full_path_file_image)
+		mypath = os.path.dirname(os.path.abspath(__file__))
+		new_name = os.path.dirname(os.path.abspath(__file__)) + '/delete' + full_name_file_image + '.txt'
+		new_name = os.path.abspath (new_name)
+		command_windows = 'dir /q ' + full_path_file_image + '\\' + full_name_file_image + '>' + new_name
+		info_file = os.system(command_windows)
+		try:
+			file_info = open(new_name)
+			lines = file_info.readlines()
+			info_file_owner = (lines[5].rstrip("\n")).split(' ')
+			self.file_owner = (info_file_owner[len(info_file_owner)-2]).split("\\")
+			file_info.close()
+			os.remove(new_name)
+		except IOError:
+				print("File cannot open" + new_name)
 
 
 	def is_valid_image(self,full_path_file_image,full_name_file_image):
@@ -94,6 +111,10 @@ class ImageFile():
 		full_path_file_image -- path of image
 		full_name_file_image -- file name image
 
+		Return Value:
+		True -- if image file is a valid image: exist and type image is supported by ImageFile
+		False -- If the image file is not valid: the file doesn't exist or 
+				 type image is not supported by ImageFile
 		"""
 		full_path_image = full_path_file_image + full_name_file_image
 		if os.path.isfile(full_path_image):#file exist		
@@ -112,11 +133,11 @@ class ImageFile():
 		"""Return location(path) and name.extension of image"""
 		return (os.path.join(self.get_path(),self.get_complete_image_with_type()))
 
-	def get_name(self):
+	def get_file_name(self):
 		"""Return name of imageFile"""
 		return self.file_name
 
-	def set_name(self, file_name_image):
+	def set_file_name(self, file_name_image):
 		""" Set name to imageFile
 		Keyword arguments:
 		file_name image-- filename of image file
@@ -135,29 +156,21 @@ class ImageFile():
 		self.file_size_high = size_high_image
 		self.file_size_width = size_width_image
 
-	def get_size_high(self):
+	def get_file_size_high(self):
 		"""Return size(high) of imageFile"""
 		return self.file_size_high
 
-	def get_size_width(self):
+	def get_file_size_width(self):
 		"""Return size(with in pixels) of imageFile"""
 		return self.file_size_width
 
-	def set_owner(self, owner_file_image):
-		""" Set owner to imageFile
-		
-		Keyword arguments:
-		owner_file_image-- owner image file
-		
-		"""
-		self.file_owner = owner_file_image
-
-	def get_owner(self):
+	
+	def get_file_owner(self):
 		"""Return owner of imageFile"""
 		return self.file_owner
 
 
-	def set_size_image_KB(self, size_image_KB):
+	def set_size_KB_image(self, size_image_KB):
 		""" Set sizein KB to imageFile
 		
 		Keyword arguments:
@@ -166,16 +179,16 @@ class ImageFile():
 		"""
 		self.size_KB_image = size_image_KB
 
-	def get_size_image_KB(self):
+	def get_size_KB_image(self):
 		"""Return size in KB of imageFile"""
 		return self.size_KB_image
 
 
-	def get_type(self):
+	def get_file_type(self):
 		"""Return type of imageFile"""
 		return self.file_type
 
-	def set_type(self, file_type_image):
+	def set_file_type(self, file_type_image):
 		""" Set type to imageFile
 		
 		Keyword arguments:
@@ -185,7 +198,7 @@ class ImageFile():
 		self.file_type = file_type_image
 
 	
-	def set_path(self, file_path_image):
+	def set_file_path(self, file_path_image):
 		""" Set path to imageFile
 		
 		Keyword arguments:
@@ -194,7 +207,7 @@ class ImageFile():
 		"""
 		self.file_path = file_path_image
 
-	def get_path(self):
+	def get_file_path(self):
 		"""Return path of imageFile"""
 		return self.file_path
 
