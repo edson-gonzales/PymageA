7efea86be140e0881fc753a0f5b31ae9ee23dea2
Yasmina Terrazas <yasmina.terrazas@jalasoft.com>
2014-02-18 01:43:29 -0400

    Add implementation to search duplicates and unites test

--- main/modules/searcher_module/search_by_name.py
+++ main/modules/searcher_module/search_by_name.py
@@ -7,13 +7,16 @@ class SearchDuplicatesByName(GeneralSearch):
 		
 		"""
 		list_of_duplicates = []
+		list_to_compare = list_of_images_to_look_for
 		for image_object in list_of_images_to_look_for:
-			for image_object_compare in list_of_images_to_look_for:
-				num_of_copies = 0
+			num_of_copies = 0
+			for image_object_compare in list_to_compare:
 				if image_object_compare.get_complete_image_with_type() == image_object.get_complete_image_with_type():
 					num_of_copies = num_of_copies + 1
 			if num_of_copies > 1:
-				list_of_duplicates.append(image_object_compare)
+				list_of_duplicates.append(image_object)
+				list_to_compare.remove(image_object)
 				
+		
 		return list_of_duplicates
 	
--- tests/test_modules/test_search_by_name/test_search_by_name.py
+++ tests/test_modules/test_search_by_name/test_search_by_name.py
@@ -14,18 +14,35 @@ class TestSearchByName(unittest.TestCase):
 		self.test_path, not_needed_current_dir = os.path.split(os.getcwd())
 		self.search_by_name = SearchDuplicatesByName()
 		self.list_images = ListImages()
-			
+		self.message = ""
+
+	def are_items_equal(self, list_of_images_expected, list_of_images_obtained):
+		are_equal = True
+		if len(list_of_images_expected) == len(list_of_images_obtained):
+			for image_object in list_of_images_obtained:
+				if image_object.get_complete_image_with_type() not in list_of_images_expected:
+					are_equal = False	
+					self.message = "Not equal. " + image_object.get_complete_image_with_type() + " \
+									not in " + ', '.join(list_of_images_expected)
+		else:
+			are_equal = False
+			self.message = "Not equal. The length of lists is different"
+		return are_equal		
+	
 	def test_search_by_name_returnd_a_list_of_duplicate_images(self):
 		list_of_images = []
-		self.test_path = self.test_path + "/images_for_unittest"
+		self.test_path = self.test_path + "/images_test_duplicates"
 		self.list_of_directories = self.list_images.get_all_nested_directories(self.test_path)
 		size_of_list_of_folders = len(self.list_of_directories)
-		list_of_images_duplicates_expected = []
+		list_of_images_duplicates_expected = ['balon.jpg']
 		list_of_images_obtained = self.list_images.get_all_images_from_directory \
 								(size_of_list_of_folders, list_of_images, self.list_of_directories)
-		print list_of_images_obtained
+		
 		list_of_images_dupes_by_name = self.search_by_name.search_duplicates(list_of_images_obtained)
-		self.assertEquals(list_of_images_dupes_by_name, list_of_images_duplicates_expected)
+		print len(list_of_images_duplicates_expected)
+		print len(list_of_images_dupes_by_name)
+		if self.are_items_equal(list_of_images_duplicates_expected, list_of_images_dupes_by_name) == False:
+			self.fail(self.message)
 		
 
 if __name__ == '__main__':
